6fcf2dcfccf627f051387013e0b2478edae50d03
ishwarthecodddr <ishwarthecoder@gmail.com>
2025-02-12 20:28:19 +0530

    fixed cors error

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://brainly-frontend-seven.vercel.app'
+    'https://brainly123.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
